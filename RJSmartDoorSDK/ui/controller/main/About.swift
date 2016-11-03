0327c641b91f4e7b1409f90a21fa4fdf8dd973e2
lizeyan <lizeyan@ruijiating.com>
2016-11-03 10:23:52 +0800

    code commit

--- RJSmartDoorSDK/ui/controller/main/About.swift
+++ RJSmartDoorSDK/ui/controller/main/About.swift
@@ -20,6 +20,7 @@ class About: BaseView  {
         // Do any additional setup after loading the view.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
